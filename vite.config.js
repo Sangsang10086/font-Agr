e67ca57cef8323f51bb1e0d6e5dc6ad820a14578
Sangsang10086 <1059952597@qq.com>
2025-03-03 11:04:39 +0800

    配置NGINX
    前端通过访问虚拟机中的代理来访问后端
    
    1.docker中新建NGINX
    
    docker pull nginx
    
    2.
    
    server {
    
    >     listen       80;
    >     server_name  _;
    >
    >     location / {
    >         proxy_pass http://192.168.161.1:8080;
    >         proxy_set_header Host \$host;
    >         proxy_set_header X-Real-IP \$remote_addr;
    >         proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
    >         proxy_set_header X-Forwarded-Proto \$scheme;
    >     }
    >
    >     error_page   500 502 503 504  /50x.html;
    >     location = /50x.html {
    >         root   /usr/share/nginx/html;
    >     }
    >     }
    
    ifconfig查看虚拟机ip
    
    ipconfig查看本机ip

--- vite.config.js
+++ vite.config.js
@@ -22,12 +22,12 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'http://localhost:8080/', // 后端服务器地址
+        target: 'http://192.168.161.134', // 后端服务器地址
         changeOrigin: true, // 是否改变请求域名,本地会虚拟一个服务器接受请求并代你发送请求，这样就不会有跨域问题（只适合开发环境）
-        headers:{
-          host: 'localhost:8080',
-          origin:'http://localhost:8080'
-        },
+        // headers:{
+        //   host: '192.168.161.134',
+        //   origin:'http://192.168.161.134'
+        // },
         rewrite: (path) => path.replace(/^\/api/, '')//将原有请求路径中的api替换为''
 
       },
